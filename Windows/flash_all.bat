7aad6a53ae29fa730345f76603c4b917c4e7e703
HELLBOY017 <abhaygill017@gmail.com>
2024-01-14 17:49:21 +0700

    Windows: Add ability to run as admin
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

--- Windows/flash_all.bat
+++ Windows/flash_all.bat
@@ -1,6 +1,4 @@
 @echo off
-setx PATH "%PATH%;%cd"
-
 title Transsion mt6789 Fastboot ROM Flasher
 
 echo ###########################################################
@@ -11,10 +9,13 @@ echo #        [From Nothing Phone (2) Telegram Dev Team]       #
 echo #          Adapted for Transsion mt6789 by rama982        #
 echo ###########################################################
 
+cd %~dp0
+set fastboot=.\fastboot.exe
+
 echo #############################
 echo # CHANGING ACTIVE SLOT TO A #
 echo #############################
-fastboot --set-active=a
+%fastboot% --set-active=a
 
 echo ###################
 echo # FORMATTING DATA #
@@ -22,8 +23,8 @@ echo ###################
 choice /m "Wipe Data?"
 if %errorlevel% equ 1 (
     echo Please ignore "Did you mean to format this partition?" warnings.
-    fastboot erase userdata
-    fastboot erase metadata
+    %fastboot% erase userdata
+    %fastboot% erase metadata
 )
 
 echo #################
@@ -31,27 +32,27 @@ echo # FLASHING BOOT #
 echo #################
 for %%i in (boot vendor_boot dtbo) do (
     for %%s in (a b) do (
-        fastboot flash %%i_%%s %%i.img
+        %fastboot% flash %%i_%%s %%i.img
     )
 )
 
 echo ##########################
 echo # REBOOTING TO FASTBOOTD #
 echo ##########################
-fastboot reboot fastboot
+%fastboot% reboot fastboot
 
 echo #####################
 echo # FLASHING FIRMWARE #
 echo #####################
 for %%i in (dpm gz lk mcupm md1img pi_img scp spmfw sspm tee) do (
     for %%s in (a b) do (
-        fastboot flash %%i_%%s %%i.img
+        %fastboot% flash %%i_%%s %%i.img
     )
 )
-fastboot flash preloader_a preloader_raw.img
-fastboot flash preloader_b preloader_raw.img
-fastboot flash logo_a logo.bin
-fastboot flash logo_b logo.bin
+%fastboot% flash preloader_a preloader_raw.img
+%fastboot% flash preloader_b preloader_raw.img
+%fastboot% flash logo_a logo.bin
+%fastboot% flash logo_b logo.bin
 
 echo ###################
 echo # FLASHING VBMETA #
@@ -59,21 +60,21 @@ echo ###################
 choice /m "Disable android verified boot?, If unsure, say N. Bootloader won't be lockable if you select Y."
 if %errorlevel% equ 1 (
     set disable_avb=1
-    fastboot flash vbmeta --disable-verity --disable-verification vbmeta.img
+    %fastboot% flash vbmeta --disable-verity --disable-verification vbmeta.img
 ) else (
-    fastboot flash vbmeta vbmeta.img
+    %fastboot% flash vbmeta vbmeta.img
 )
 
 echo ##################
 echo # FLASHING SUPER #
 echo ##################
-fastboot flash super super.img
+%fastboot% flash super super.img
 
 echo #################################
 echo # FLASHING VBMETA SYSTEM/VENDOR #
 echo #################################
 for %%i in (vbmeta_system vbmeta_vendor) do (
-    fastboot flash %%i_a %%i.img
+    %fastboot% flash %%i_a %%i.img
 )
 
 echo #############
@@ -81,7 +82,7 @@ echo # REBOOTING #
 echo #############
 choice /m "Reboot to system? If unsure, say Y."
 if %errorlevel% equ 1 (
-    fastboot reboot
+    %fastboot% reboot
 )
 
 echo ########
